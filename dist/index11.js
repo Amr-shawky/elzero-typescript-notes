b569ebf4f26c6071a9cbfbd339aedce8528abca0
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-07-13 14:27:03 +0300

    add TypeScript implementation of type aliases and improve compare function with type annotations

---
+++ dist/index11.js
@@ -0,0 +1,12 @@
+"use strict";
+let zName = "Osama";
+let all9 = 10;
+all9 = "Hello";
+let status1 = "active";
+let causes1 = 1;
+function compare(a, b) {
+    if (a === b)
+        return 0;
+    return a > b ? 1 : -1;
+}
+//# sourceMappingURL=index11.js.map
